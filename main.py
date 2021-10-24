8b00797075687c2a32262d801c71974579b3b7c3
User <funikov.1997@mail.ru>
2021-10-24 10:35:07 +0300

    Программа шифрования и дешифрования текста методом Цезаря

---
+++ main.py
@@ -0,0 +1,47 @@
+
+# Получаем алфавит на английском языке
+# Пустой массив
+alphabets_in_lowercase = []
+for i in range(97, 123):
+    # Заполняем его символами, обращаясь к ним по коду
+    alphabets_in_lowercase.append(chr(i))
+
+print('Текущий алфавит: ' + str(alphabets_in_lowercase))
+crypto = str(input('Введите строку для шифрования: '))
+crypto_position = int(input('Укажите позицию смещения алфавита: '))
+print('Строка для шифрования: ' + crypto)
+
+# Преобразуем введённую строку в нижний регистр
+crypto_string = crypto.lower()
+
+crypto_array = list(crypto_string)
+
+array_after_crypto = []
+
+for i in range(0, len(crypto_array)):
+    new_crypto_index = alphabets_in_lowercase.index(crypto_array[i])
+    if new_crypto_index < len(alphabets_in_lowercase) - crypto_position:
+        array_after_crypto.append(alphabets_in_lowercase[new_crypto_index + crypto_position])
+    else:
+        array_after_crypto.append(alphabets_in_lowercase[0 + crypto_position])
+
+string_after_crypto = ''.join(array_after_crypto)
+print('Шифрованная строка ' + string_after_crypto)
+
+print("Введите 1, если надо выполнить дешифрование, иначе введите 0")
+flag = int(input())
+
+crypto_array = array_after_crypto
+array_after_de_crypto = []
+if flag == 1:
+    for i in range(0, len(crypto_array)):
+        new_crypto_index = alphabets_in_lowercase.index(crypto_array[i])
+        # if new_crypto_index < len(alphabets_in_lowercase) - crypto_position:
+        array_after_de_crypto.append(alphabets_in_lowercase[new_crypto_index - crypto_position])
+        # else:
+        #     array_after_crypto.append(alphabets_in_lowercase[0 + crypto_position])
+    string_after_de_crypto = ''.join(array_after_de_crypto)
+    print('Дешифрованная строка: ' + string_after_de_crypto)
+    print('Дешифрование завершено')
+else:
+    exit('Шифрование завершено')
